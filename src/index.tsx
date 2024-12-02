69b16fb0ac4e36f980a0ae02f6c17b5ea07ef537
Vladimir Tomic <vladimir.tomic@financescout24.ch>
2024-12-02 08:26:58 +0100

    feat: add speed insights

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
       <App />
+      <SpeedInsights />
     </ChakraProvider>
   </React.StrictMode>
 );
